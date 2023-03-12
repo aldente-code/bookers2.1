0ae61259ab164f3bf6693b9d811c94005da38a47
EC2 Default User <ec2-user@ip-172-31-33-54.ap-northeast-1.compute.internal>
2023-03-12 11:02:44 +0000

    [Finish] bookers2.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
-    name: "メンター太郎",
-    email: "a@a",
-    password: "aaaaaa"
-)
-
-5.times do |n|
-    User.create!(
-        name: Faker::Name.first_name,
-        email: Faker::Internet.email,
-        introduction: Faker::Lorem.characters(number: 20),
-        password: Faker::Lorem.characters(number: 10)
-    )
-    
-    Book.create!(
-        title: Faker::Book.title,
-        body: "ランダム#{Faker::Lorem.characters(number: 5)}",
-        user_id: 1
-        )
-end
-Favorite.create!(
-    user_id: 1,
-    book_id: 1,
-)
-
